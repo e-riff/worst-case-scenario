6a57d60b465cb738c5dc39a2099e6b61a34ec43a
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-11-24 17:13:22 +0100

    Lien régions + image + retravail formulaire AVEC user_id par défaut

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Model\AddItemManager;
+use App\Model\ItemManager;
+use App\Model\RegionManager;
 
 class HomeController extends AbstractController
 {
@@ -16,6 +17,23 @@ class HomeController extends AbstractController
 
     public function addItem(): string
     {
-        return $this->twig->render('Home/add_item.html.twig');
+        $errors = [];
+        $regionManager = new RegionManager();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $item = array_map('trim', $_POST);
+            $item = array_map('htmlentities', $item);
+            $itemManager = new ItemManager();
+            $itemManager->addItem($_POST);
+            header('Location: /validation');
+        }
+        return $this->twig->render('Home/add_item.html.twig', [
+            'regions' => $regionManager->selectAll(),
+            'errors' => $errors
+        ]);
+    }
+
+    public function validation(): string
+    {
+        return $this->twig->render('Home/validation.html.twig');
     }
 }
--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -2,21 +2,22 @@
 
 namespace App\Model;
 
-class AddItemManager extends AbstractManager
+class ItemManager extends AbstractManager
 {
     public const TABLE = 'item';
 
     public function addItem(array $item)
     {
         $statement = $this->pdo->prepare("INSERT INTO " . static::TABLE .
-            " (`name`, `adress`, `region_id`, `image`, `description` `user_id` `postcode`) 
-        VALUES (:name, :adress, :region, :image, :description, :id, :postcode)");
+            " (`name`, `address`, `region_id`, `image`, `description`, `user_id`, `postcode`) 
+        VALUES (:name, :address, :region_id, :image, :description, :id, :postcode)");
         $statement->bindValue(':name', $item['title'], \PDO::PARAM_STR);
-        $statement->bindValue(':adress', $item['address'], \PDO::PARAM_STR);
-        $statement->bindValue(':region', $item['region'], \PDO::PARAM_INT);
+        $statement->bindValue(':address', $item['address'], \PDO::PARAM_STR);
+        $statement->bindValue(':region_id', $item['region'], \PDO::PARAM_INT);
         $statement->bindValue(':image', $item['image'], \PDO::PARAM_STR);
         $statement->bindValue(':description', $item['description'], \PDO::PARAM_STR);
         $statement->bindValue(':postcode', $item['postcode'], \PDO::PARAM_INT);
+        $statement->bindValue(':id', $item['id'], \PDO::PARAM_INT);
         $statement->execute();
         return $this->pdo->lastInsertId();
     }
