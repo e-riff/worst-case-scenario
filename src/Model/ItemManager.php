2e3ead66e0c1fa286fd519db32f0269a45ae8fba
e-riff <emeric.riff@gmail.com>
2022-11-25 10:06:21 +0100

    counter ok

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -13,7 +13,6 @@ class ItemManager extends AbstractManager
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
             " (`name`, `address`, `region_id`, `image`, `description`, `user_id`, `postcode`)
         VALUES (:name, :address, :region_id, :image, :description, :id, :postcode)");
-
         $statement->bindValue(':name', $item['title'], \PDO::PARAM_STR);
         $statement->bindValue(':address', $item['address'], \PDO::PARAM_STR);
         $statement->bindValue(':region_id', $item['region'], \PDO::PARAM_INT);
@@ -29,7 +28,7 @@ class ItemManager extends AbstractManager
     public function selectAllWithLike()
     {
         $statement = $this->pdo->query("SELECT i.*, l.id AS like_Id, l.good_or_bad  FROM " . self::TABLE . " AS i " .
-            "LEFT JOIN like_or_dislike AS l ON i.id=l.item_id");
+            "LEFT JOIN like_or_dislike AS l ON i.id=l.item_id AND l.user_id=" . $_SESSION['user_id']);
         return $statement->fetchAll();
     }
 }
