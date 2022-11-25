072046e0a8421b6c89004fed857f6594dd8e2273
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-11-25 10:35:59 +0100

    Essai pour lier regions + categories

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -11,8 +11,8 @@ class ItemManager extends AbstractManager
     public function addItem(array $item)
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE .
-            " (`name`, `address`, `region_id`, `image`, `description`, `user_id`, `postcode`)
-        VALUES (:name, :address, :region_id, :image, :description, :id, :postcode)");
+            " (`name`, `address`, `region_id`, `image`, `description`, `user_id`, `postcode`, category_id)
+        VALUES (:name, :address, :region_id, :image, :description, :id, :postcode, :category)");
 
         $statement->bindValue(':name', $item['title'], \PDO::PARAM_STR);
         $statement->bindValue(':address', $item['address'], \PDO::PARAM_STR);
@@ -28,8 +28,8 @@ class ItemManager extends AbstractManager
 
     public function selectAllWithLike()
     {
-        $statement = $this->pdo->query("SELECT i.*, l.id AS like_Id, l.good_or_bad  FROM " . self::TABLE . " AS i " .
-            "LEFT JOIN like_or_dislike AS l ON i.id=l.item_id");
+        $statement = $this->pdo->query("SELECT i.*, l.id AS like_Id, l.good_or_bad, region.name FROM " . self::TABLE . " AS i " .
+            "LEFT JOIN like_or_dislike AS l ON i.id=l.item_id INNER JOIN region ON i.region_id=region.id");
         return $statement->fetchAll();
     }
 }
