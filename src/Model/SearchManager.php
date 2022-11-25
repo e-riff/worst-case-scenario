bec60b55114659407759df6453b3f636f584ec5b
e-riff <emeric.riff@gmail.com>
2022-11-25 11:49:45 +0100

    search improved

--- src/Model/SearchManager.php
+++ src/Model/SearchManager.php
@@ -8,12 +8,13 @@ class SearchManager extends AbstractManager
 
     public function searchEngine(string $research)
     {
-            $statement = $this->pdo->prepare("SELECT i.*, l.id AS like_Id, l.good_or_bad  FROM " .
+        $statement = $this->pdo->prepare("SELECT i.*, l.id AS like_Id, l.good_or_bad  FROM " .
             self::TABLE . " AS i " .
             "LEFT JOIN like_or_dislike AS l ON i.id=l.item_id AND l.user_id=" . $_SESSION['user_id'] .
-            " WHERE i.name LIKE :research");
-            $statement->bindValue(':research', "%" . $research . "%", \PDO::PARAM_STR);
-            $statement->execute();
+            " WHERE i.name LIKE :research OR i.address LIKE :test");
+        $statement->bindValue(':research', "%" . $research . "%", \PDO::PARAM_STR);
+        $statement->bindValue(':test', "%$research%", \PDO::PARAM_STR);
+        $statement->execute();
         return $statement->fetchAll();
     }
 }
